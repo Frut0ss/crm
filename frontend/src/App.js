3aaef3e2427d2f36d0cdbc5ffab2aedc8e0c8eed
Frut0ss <alexfrutos8@gmail.com>
2025-09-20 10:45:40 +0100

    Fix ESLint error: remove unused Navigate import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Customers from './Customers';
 import Bookings from './Bookings';
 import './App.css';
